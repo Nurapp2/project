3aeb18c5f0472945d63ed96d984a7a2db85d70e8
Nurapp2 <Nurbakyt_017@outlook.com>
2025-06-03 12:19:36 +0500

    Add files via upload

---
+++ LAB1/1.py
@@ -0,0 +1,29 @@
+print("hello World")
+
+if 5 > 2:
+    print("Five is greater than two!")
+  
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+x = 3
+print(type(x))
+
+
+x = 1
+y = 1.2
+z = 1j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = int(1)  
+y = int(2.8)  
+z = int("3")  
---
+++ LAB1/Strings.py
@@ -0,0 +1,27 @@
+#Strings1
+x = "Hello World"
+print(len(x))
+
+#Strings2
+txt = "Hello World"
+x = txt[0]
+
+#Strings3
+txt = "Hello World"
+x = txt[2:5]
+
+#Strings4
+txt = "Hello World"
+x = txt.strip()
+
+#Strings5
+txt = "Hello World"
+txt = txt.upper()
+
+#Strings6
+txt = "Hello World"
+txt = txt.lower()
+
+#Strings7
+txt = "Hello World"
+txt = txt.replace("H", "J")
