64e6735ceedcbcb68de0a0be1a76fe6053d8ea3c
Nurapp2 <Nurbakyt_017@outlook.com>
2025-06-04 11:38:40 +0500

    Add files via upload

---
+++ LAB2/Operators.py
@@ -0,0 +1,16 @@
+#Sum x + y	
+#Subtraction	x - y	
+#Multiplication	x * y	
+#Division	x / y	
+#Modulus	x % y	
+#Exponentiation	x ** y	
+#Floor division	x // y
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+
+if 5 != 10:
+  print("5 and 10 is not equal")
+
+if 5 == 10 or 4 == 4:
+  print("At least one of the statements is true")
